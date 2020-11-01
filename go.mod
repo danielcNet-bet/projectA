7a06787bb6e4160533c9b77cf2b692fbf8c55148
DanielC <danielc@net-bet.net>
2020-11-01 20:48:26 +0000

    Init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/danielcNet-bet/projectA
+
+go 1.15
+
+require github.com/danielcNet-bet/projectB v0.1.0
---
+++ useless/useless.go
@@ -0,0 +1,11 @@
+package useless
+
+import "github.com/danielcNet-bet/projectB"
+
+func FuncOne() {
+	projectB.GetNumber()
+}
+
+func FuncTwo() {
+
+}
